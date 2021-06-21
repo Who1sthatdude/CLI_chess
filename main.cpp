6fa7640f1613352d2ef6ba0e38a0047482bd9712
Who1sthatdude <goodplayer322@gmail.com>
2021-06-21 19:47:14 +0300

    reworked parseInput() function

--- main.cpp
+++ main.cpp
@@ -34,22 +34,33 @@ void parseInput(std::string &str) {
     if (str.length() != 4) {
         throw WrongInputException("Wrong input, please follow the instructions");
     }
-    try {
-        MoveCoord::prevRow = (int) str[0] - 97;
-        MoveCoord::prevCol = str[1] - '0' - 1;
-        MoveCoord::nextRow = (int) str[2] - 97;
-        MoveCoord::nextCol = str[3] - '0' - 1;
-    } catch (std::exception &e) {
-        std::cout << e.what() << std::endl;
-    }
+    MoveCoord::prevRow = (int) str[0] - 97;
+    MoveCoord::prevCol = str[1] - '0' - 1;
+    MoveCoord::nextRow = (int) str[2] - 97;
+    MoveCoord::nextCol = str[3] - '0' - 1;
+    // checking whether the coords numbers are right, don't even try to read that
+    if (((MoveCoord::prevRow < 0) || (MoveCoord::prevRow >= 8)) ||
+        ((MoveCoord::nextRow < 0) || (MoveCoord::nextRow >= 8)) ||
+        ((MoveCoord::prevCol < 0) || (MoveCoord::prevCol >= 8)) ||
+        ((MoveCoord::nextCol < 0) || (MoveCoord::nextCol >= 8)))
+        throw WrongInputException("Wrong input, please follow the instructions");
 }
 
 int main() {
     std::string str;
-    std::getline(std::cin, str, '\n');
-    parseInput(str);
     Board *board = Board::GetInstance();
     board->Initialize();
     board->show();
+    while(true){
+        std::cout<<"Enter input in format (A-H 1-8) (A-H 1-8)"<<std::endl;
+        std::getline(std::cin, str, '\n');
+        try {
+            parseInput(str);
+        }catch (WrongInputException &e){
+            std::cout<<e.what()<<std::endl;
+        }
+    }
+
+
     return 0;
 }
